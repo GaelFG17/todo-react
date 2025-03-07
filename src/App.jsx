5afad4ed8958f271353eb07ec386aa3395e14a76
GaelFG17 <gaelfg1720@gmail.com>
2025-03-07 02:42:31 -0600

    Implementacion del form para añadir tareas

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,13 @@ import Formulario from './Formulario';
 import Tareas from './Tareas';
 
 const App = () => {
-  const [listaTareas, setListaTareas] = useState([
-    {
-      titulo : "Tarea 1",
-      descripccion : "Descripcion de la tarea 1",
-      isComplete : true,
-    },
-    {
-      titulo : "Tarea 2",
-      descripccion : "Descripcion de la tarea 2",
-      isComplete : false,
-    },
-    {
-      titulo : "Tarea 3",
-      descripccion : "Descripcion de la tarea 3",
-      isComplete : true,
-    },
-    {
-      titulo : "Tarea 4",
-      descripccion : "Descripcion de la tarea 4",
-      isComplete : false,
-    },
-    {
-      titulo : "Tarea 5",
-      descripccion : "Descripcion de la tarea 5",
-      isComplete : true,
-    },
-    {
-      titulo : "Tarea 6",
-      descripccion : "Descripcion de la tarea 6",
-      isComplete : false,
-    }
-  ])
+  const [listaTareas, setListaTareas] = useState([])
   return (
     <>
       <Header/>
-      <Formulario setListaTareas = {setListaTareas}/>
+      <Formulario 
+      listaTareas = {listaTareas}
+      setListaTareas = {setListaTareas}/>
       <Tareas 
         listaTareas = {listaTareas} 
         setListaTareas = {setListaTareas}
